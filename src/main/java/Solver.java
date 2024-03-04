e1783f7e11fbde1e32d3c07f461ad5b6495ce6f7
Ermolovq <145741855+Ermolovq@users.noreply.github.com>
2024-03-04 22:17:49 +0200

    Delete src directory

--- src/main/java/Solver.java
+++
@@ -1,6 +0,0 @@
-public class Solver {
-    public CalculationResult solve(double parameter1, double parameter2) {
-        double result = parameter1 + parameter2;
-        return new CalculationResult(parameter1, parameter2, result);
-    }
-}
