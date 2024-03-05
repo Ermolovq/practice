d5818047f49f5608bec0c981dba0652f85d38416
Ermolovq <145741855+Ermolovq@users.noreply.github.com>
2024-03-05 23:53:58 +0200

    practice_04-03

---
+++ practice_04-03/practice/src/main/java/com/mycompany/practice/Practice.java
@@ -0,0 +1,78 @@
+package com.mycompany.practice;
+
+import java.io.*;
+import java.util.Scanner;
+public class Practice {
+    public static void main(String[] args) {
+        boolean i = true;
+        while(i){
+            System.out.prinln("Type:\n '1' - change your number to binary and count alternations of 0 and 1");
+            int 
+        }
+        int num = 1541;
+        SerializableClass objectToSerialize = new SerializableClass(num, BinaryAlternation(num));
+        // Серіалізація об'єкту
+        serializeObject(objectToSerialize);
+
+        // Десеріалізація об'єкту
+        SerializableClass deserializedObject = deserializeObject();
+
+        // Виведення десеріалізованого об'єкту
+        System.out.println("Deserialized object:");
+        System.out.println("Number: " + deserializedObject.getNum());
+        System.out.println("Result: " + deserializedObject.getResult());
+    }
+
+    // Метод для серіалізації об'єкту
+    private static void serializeObject(SerializableClass object) {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("data.ser"))) {
+            outputStream.writeObject(object);
+            System.out.println("Object serialized");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Метод для десеріалізації об'єкту
+    private static SerializableClass deserializeObject() {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("data.ser"))) {
+            SerializableClass object = (SerializableClass) inputStream.readObject();
+            System.out.println("Object deserialized");
+            return object;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public static int BinaryAlternation(int decimalNumber){
+        // Перетворення десяткового числа на його двійковий варіант
+        String binaryNumber = Integer.toBinaryString(decimalNumber);
+
+        // Підрахунок кількості чергувань 0 та 1
+        int alternations = countAlternations(binaryNumber);
+
+        // Виведення результату
+        System.out.println("The number of alternations of 0 and 1 in the binary representation of a number " + decimalNumber + " is: " + alternations);
+        
+        return alternations;
+    }
+
+    // Метод для підрахунку кількості чергувань 0 та 1
+    private static int countAlternations(String binaryNumber) {
+        int alternations = 0;
+
+        // Проходимося по кожному біту у двійковому представленні
+        for (int i = 1; i < binaryNumber.length(); i++) {
+            // Якщо поточний біт відрізняється від попереднього, збільшуємо лічильник чергувань
+            if (binaryNumber.charAt(i) != binaryNumber.charAt(i - 1)) {
+                alternations++;
+            }
+        }
+        
+        return alternations;
+    }
+    
+}
+
+
---
+++ practice_04-03/practice/src/main/java/com/mycompany/practice/SerializableClass.java
@@ -0,0 +1,30 @@
+package com.mycompany.practice;
+
+import java.io.Serializable;
+
+class SerializableClass implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private float num;
+    private int result;
+
+    public SerializableClass(float num, int result) {
+        this.num = num;
+        this.result = result;
+    }
+    public SerializableClass(float num){
+        this.num = num;
+        this.result = 0;
+    }
+
+    public float getNum() {
+        return num;
+    }
+    public int getResult() {
+        return result;
+    }
+    
+    public void setResult(int number) {
+        this.result = number;
+    }
+}
